17cb757acc7a8df1b8e942ab0451de95514446e2
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-05-31 12:29:59 +0530

    component fragmentation

--- starting_project_1/src/App.jsx
+++ starting_project_1/src/App.jsx
@@ -1,45 +1,14 @@
-import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
-import TabButton from "./components/TabButton";
-import { useState } from "react";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("PLease select a topic");
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic + "1");
-  }
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcepts
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {selectedTopic}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </div>
   );
--- starting_project_1/src/components/CoreConcepts.jsx
+++ starting_project_1/src/components/CoreConcepts.jsx
@@ -1,9 +1,15 @@
-export default function CoreConcepts({ image, description, title }) {
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "../components/CoreConcept";
+
+export default function CoreConcepts() {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItems) => (
+          <CoreConcept key={conceptItems.title} {...conceptItems} />
+        ))}
+      </ul>
+    </section>
   );
 }
