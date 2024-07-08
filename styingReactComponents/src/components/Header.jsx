d3b4215d4cec1875917089afa7de0a2245e9da0d
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-07-08 10:06:11 +0530

    button components made with tailwind

--- styingReactComponents/src/components/Header.jsx
+++ styingReactComponents/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         alt="A canvas"
         className="object-contain w-44 h-44 mb-8"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactArt
       </h1>
       <p className="text-stone-500">A community of artists and art-lovers.</p>
