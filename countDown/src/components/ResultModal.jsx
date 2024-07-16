6dd625b7bc874545e302cf350456dd21dd8afc12
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-07-16 14:19:42 +0530

    success message and onClose added

--- countDown/src/components/ResultModal.jsx
+++ countDown/src/components/ResultModal.jsx
@@ -8,6 +8,7 @@ const ResultModal = forwardRef(function ResultModal(
   const dialog = useRef();
   const userLost = remainingTime <= 0;
   const formattedTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -18,15 +19,16 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime}</strong> seconds.
       </p>
       <p>
         You stopped time with <strong>{formattedTime}seconds left</strong>
       </p>
-      <form action="dialog" onSubmit={onReset}>
+      <form action="dialog">
         <button>Close</button>
       </form>
     </dialog>
