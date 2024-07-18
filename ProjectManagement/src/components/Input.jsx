4b4ee490f77696949315ed765cd704d755058973
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-07-19 00:37:02 +0530

    input styling

--- ProjectManagement/src/components/Input.jsx
+++ ProjectManagement/src/components/Input.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Input = ({ textArea, label, ...props }) => {
+  const inputClases =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-      <label htmlFor="">{label}</label>
-      {textArea ? <textArea {...props} /> : <input {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <label htmlFor="" className="text-sm uppercase text-stone-500 font-bold">
+        {label}
+      </label>
+      {textArea ? (
+        <textArea className={inputClases} {...props} />
+      ) : (
+        <input className={inputClases} {...props} />
+      )}
     </p>
   );
 };
--- ProjectManagement/src/components/NewProject.jsx
+++ ProjectManagement/src/components/NewProject.jsx
@@ -3,13 +3,17 @@ import Input from "./Input";
 
 const NewProject = () => {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button>Cancle</button>
+          <button className="tracking-wider text-stone-800 hover:text-stone-950 ">
+            Cancle
+          </button>
         </li>
         <li>
-          <button>Save</button>
+          <button className="tracking-wider py-2 px-6 rounded bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
         </li>
       </menu>
       <div>
